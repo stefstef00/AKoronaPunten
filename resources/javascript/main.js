c74514473f8a927a62f56a92a0bff53f6877e441
Stef Rasing <S.M.Rasing@student.tudelft.nl>
2020-06-09 17:40:16 +0200

    Added backend to frontend support

---
+++ resources/javascript/main.js
@@ -0,0 +1,10 @@
+function setQuestion(str) {
+    $("#question").text(str);
+}
+
+function setAnswers(str1, str2, str3, str4) {
+    $("#answer1").text(str1);
+    $("#answer2").text(str2);
+    $("#answer3").text(str3);
+    $("#answer4").text(str4);
+}
